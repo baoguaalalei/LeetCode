f02a41225ae95a3a8f9ddd7b6fd40f3b8de5e645
baoguaalalei <wenhuixu@tju.edu.cn>
2017-10-10 10:47:38 +0800

    “计算字符串的回文数

---
+++ PalindromicSubstrings.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stdlib.h>
+using namespace std;
+class Solution{
+public:
+	int countSubstrings(string s){
+		int num;//记录回文数
+		int size_num;//保存字符串的大小
+	    num = 0;
+		size_num = s.size();
+		for (int i = 0;i < size_num;i++)
+		   for(int j = i;j < size_num;j++){
+			   if (j==i)
+			   {
+				   num++;
+			   }
+			   if (j>i)
+			   {
+				   if (s[j]!=s[i]) continue;
+				   else
+				   {
+					   int kuadu = j-i;
+					   for (int p = 0;p <= kuadu/2; p++)
+					   {
+						   if (p==kuadu/2&&(s[i+p]==s[j-p])) 
+						   {
+							   num++;
+							  // printf("%c %d\n",s[i+p],i+p);
+							   break;
+						   }
+						   if (s[i+p]!=s[j-p]) break;
+					   }
+				   }
+			   }
+		   }
+		   return num;
+	}
+};
+
+int main(){
+	string s = "xkjkqlajprjwefilxgpdpebieswu";
+	Solution soul;
+	int num = soul.countSubstrings(s);
+	printf("The result is %d",num);
+	return 0;
+}
